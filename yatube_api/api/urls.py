6098ec99d45f31edec87ed48eb4dfc16a3d3d23f
Дмитрий <d77s@inbox.ru>
2023-11-11 20:13:24 +0300

    Next

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -13,8 +13,9 @@ from rest_framework.authtoken import views
 #                    )
 
 urlpatterns = [
-#    path('v1/', include(router_v1.urls)),
-    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     # superuser: login/pass admin1/admin1
     # {"token":"d6db9c9a47f413dc44f95a4bac1e9581e19af99f"}
+    #    path('v1/', include(router_v1.urls)),
 ]
