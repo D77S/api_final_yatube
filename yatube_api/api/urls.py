1c6857357f7a7807dc38350cf207c9c2f947e216
Дмитрий <d77s@inbox.ru>
2023-11-11 19:57:51 +0300

    Next

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+# from .views import PostViewSet, GroupViewSet, CommentViewSet
+
+# router_v1 = routers.DefaultRouter()
+# router_v1.register('posts', PostViewSet, basename='posts')
+# router_v1.register('groups', GroupViewSet, basename='groups')
+# router_v1.register(r'posts/(?P<post_id>\d+)/comments',
+#                    CommentViewSet,
+#                    basename='comments'
+#                    )
 
 urlpatterns = [
+#    path('v1/', include(router_v1.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
+    # superuser: login/pass admin1/admin1
+    # {"token":"d6db9c9a47f413dc44f95a4bac1e9581e19af99f"}
 ]
