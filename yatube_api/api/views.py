75024cb05bb98394a5f1fd68774b6afb62a29995
Дмитрий <d77s@inbox.ru>
2023-11-13 19:35:40 +0300

    Next

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from posts.models import Comment, Follow, Group, Post
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import (filters,
                             mixins,
                             permissions,
                             viewsets)
-from rest_framework.pagination import LimitOffsetPagination
 
 from .permissions import IsOwnerOrReadOnly
 from .serializers import (CommentSerializer,
                           FollowSerializer,
                           GroupSerializer,
                           PostSerializer)
+
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     '''Вьюсет для групп постов.'''
