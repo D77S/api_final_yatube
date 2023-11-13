9081077c1ba86fe1e7ef906b9be7b6462bca19a4
Дмитрий <d77s@inbox.ru>
2023-11-13 21:00:15 +0300

    Next

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from posts.models import Comment, Follow, Group, Post
 from rest_framework import filters, viewsets, mixins
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from posts.models import Comment, Follow, Group, Post
 from .permissions import IsOwnerOrReadOnly
 from .serializers import (CommentSerializer,
                           FollowSerializer,
