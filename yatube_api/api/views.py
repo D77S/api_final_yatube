0df8529f7203606f353fc8d61906068805e6da43
Дмитрий <d77s@inbox.ru>
2023-11-13 23:50:51 +0300

    Next

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,11 +4,10 @@ from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from posts.models import Comment, Follow, Group, Post
 from .permissions import IsOwnerOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
-
+from posts.models import Comment, Follow, Group, Post
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
