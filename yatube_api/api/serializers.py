dc72ec82c3550639fb22169c9def0f68574c0977
Дмитрий <d77s@inbox.ru>
2023-11-13 18:11:12 +0300

    Next

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
 
+
 User = get_user_model()
 
 
